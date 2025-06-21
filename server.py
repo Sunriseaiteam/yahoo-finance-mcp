42beac36cf364d74d160c743ff19fb757c2da90b
Moshe <you@example.com>
2025-06-21 23:22:54 +0300

    chabge library

--- server.py
+++ server.py
@@ -3,7 +3,14 @@ from enum import Enum
 
 import pandas as pd
 import yfinance as yf
-from mcp.server.fastmcp import FastMCP
+# from mcp.server.fastmcp import FastMCP
+import asyncio
+import logging
+import os
+from fastmcp import FastMCP
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="[%(levelname)s]: %(message)s", level=logging.INFO)
 
 
 # Define an enum for the type of financial statement
@@ -411,7 +418,17 @@ async def get_recommendations(ticker: str, recommendation_type: str, months_back
         return f"Error: getting recommendations for {ticker}: {e}"
 
 
+# if __name__ == "__main__":
+#     # Initialize and run the server
+#     print("Starting Yahoo Finance MCP server...")
+#     yfinance_server.run(transport="stdio")
 if __name__ == "__main__":
-    # Initialize and run the server
-    print("Starting Yahoo Finance MCP server...")
-    yfinance_server.run(transport="stdio")
+    logger.info(f" MCP server started on port {os.getenv('PORT', 8080)}")
+    # Could also use 'sse' transport, host="0.0.0.0" required for Cloud Run.
+    asyncio.run(
+        yfinance_server.run_async(
+            transport="streamable-http", 
+            host="0.0.0.0", 
+            port=os.getenv("PORT", 8080),
+        )
+    )
